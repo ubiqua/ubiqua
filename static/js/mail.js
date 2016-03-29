d238bff55fd644095d5b17aa04b43e6930b4d67a
Gabriel Reges <gregesf@bluetideconsulting.com>
2016-03-29 12:05:37 -0500

    base para paginas normales

--- static/js/mail.js
+++ static/js/mail.js
@@ -15,8 +15,6 @@ jQuery(function($){
 
     // Public Keys and Values
     var form_data = {
-      "api_user": "sg_btc_ubiqua",
-      "api_key": "oiu3PSh4cENp",
       "to": "ghurtado@ubiqua.me",
       "toname": "Ubiqua",
       "subject": title,
@@ -27,9 +25,9 @@ jQuery(function($){
 
     $.ajax({
         type: 'POST',
-        url: "https://api.sendgrid.com/api/mail.send.json",
+        url: "https://formspree.io/gregesf@reseller.bluetideconsulting.com",
         data: form_data,
-        dataType: "text"
+        dataType: "json"
     })
     .always(function(){ alert("Mensaje enviado con exito"); })
     ;
